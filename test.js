4b22407020a8c9e7886e5b146c82b25fb9ba56ed
moon <moonsirl9123@gmail.com>
2021-07-30 12:08:19 +0900

    git test

---
+++ test.js
@@ -0,0 +1,18 @@
+// wecode 폴더 하위에 1부터 50의 자연수 중 짝수를 구하는 함수
+// - 내 언어(JavaScript / Python)에 맞게 파일 형식을 아래와 같이 선택해주세요.
+// - 파일 이름은 `test.js` / `test.py`
+// - 결과값은 배열에 담아 `return` 해주세요.
+var evenNumArr = []
+
+function findEvenNum(str) {
+  for (let i = 1; i >= 50; i++) {
+    if (num % 2 == 0) {
+      evenNumArr.push(str)
+      continue
+    } else {
+    }
+  }
+  return evenNumArr
+}
+
+str = [1, 2, 3, 4, 5]
