3d2f4e712d71c52e7b19c879c4fb6b6e6b7c9806
Mong <80687573+LovelyMoon23@users.noreply.github.com>
2021-07-30 19:03:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-for (let num = 1; num <= 50; num++) {
-  if (num % 2 === 0) console.log(num)
+function findEvenNum(str) {
+var evenNumArr = []
+  for (let i = 1; i <= 50; i++) {
+    if (num % 2 == 0) {
+        evenNumArr.push();
+    }
+  }
+  return evenNumArr
 }
+12:46
